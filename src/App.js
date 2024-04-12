3ca3c3c1a3307e9ff5e85501fb8cbc3921687771
m.hoseein mirzaei <1380mirza@gmail.com>
2024-04-12 10:49:05 +0330

    starting new project landing3

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button>Hello</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
